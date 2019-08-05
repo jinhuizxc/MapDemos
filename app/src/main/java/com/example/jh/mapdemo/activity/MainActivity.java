e55d87c174bba80e8e9dd44d1883c7b0f960d41c
j_h <13113017500>
2019-08-05 16:08:40 +0800

    配置不同包名配置高德地图，测试ok.

--- app/src/main/java/com/example/jh/mapdemo/activity/MainActivity.java
+++ app/src/main/java/com/example/jh/mapdemo/activity/MainActivity.java
@@ -23,6 +23,13 @@ import com.example.jh.mapdemo.utils.MapUtils;
  * 包名: com.example.jh.mapdemo
  * MyApp.jks
  * SHA1:  77:6D:B9:A7:BE:66:7B:4B:F4:7C:00:F5:77:00:14:70:1F:68:6F:92
+ *
+ * E:\Java\jdk1.8.0_201\bin>keytool -v -list -keystore E:\workspace_android\Map\MapDemos\app\MyApp.jks
+ *
+ * 包名: com.test.app1
+ * E:\Java\jdk1.8.0_201\bin>keytool -v -list -keystore E:\workspace_android\Map\MapDemos\app\app1.jks
+ * SHA1: AF:1D:6F:E0:4E:20:C6:88:A8:7E:8F:2B:32:4D:51:3E:FE:5E:B0:2D
+ *
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
